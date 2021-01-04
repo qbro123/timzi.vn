841d3cba615913618cbe605308545db31e3038f8
TuanTran <tuantq.it1307@gmail.com>
2021-01-04 19:52:20 +0700

    ver new website timzi.vn

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+(function () {
+  "use strict";
+
+  window.onload = function () {
+    window.setTimeout(fadeout, 500);
+  };
+
+  function fadeout() {
+    document.querySelector(".preloader").style.opacity = "0";
+    document.querySelector(".preloader").style.display = "none";
+  }
+
+  window.onscroll = function () {
+    var header_navbar = document.getElementById("header_navbar");
+    var sticky = header_navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+      header_navbar.classList.add("sticky");
+    } else {
+      header_navbar.classList.remove("sticky");
+    }
+
+    var backToTo = document.querySelector(".back-to-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTo.style.display = "block";
+    } else {
+      backToTo.style.display = "none";
+    }
+  };
+  var pageLink = document.querySelectorAll(".page-scroll");
+
+  pageLink.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(elem.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+        offsetTop: 1 - 60,
+      });
+    });
+  });
+  function onScroll(event) {
+    var sections = document.querySelectorAll(".page-scroll");
+    var scrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
+
+    for (var i = 0; i < sections.length; i++) {
+      var currLink = sections[i];
+      var val = currLink.getAttribute("href");
+      var refElement = document.querySelector(val);
+      var scrollTopMinus = scrollPos + 73;
+      if (
+        refElement.offsetTop <= scrollTopMinus &&
+        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+      ) {
+        document.querySelector(".page-scroll").classList.remove("active");
+        currLink.classList.add("active");
+      } else {
+        currLink.classList.remove("active");
+      }
+    }
+  }
+
+  window.document.addEventListener("scroll", onScroll);
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  var navbarCollapse = document.querySelector(".navbar-collapse");
+
+  document.querySelectorAll(".page-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+  });
+
+  var cu = new counterUp({
+    start: 0,
+    duration: 2000,
+    intvalues: true,
+    interval: 100,
+    append: "k",
+  });
+  cu.start();
+
+  var wow = new WOW({
+    mobile: false,
+  });
+  wow.init();
+})();
